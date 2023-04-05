6db2cd6d5d0dc0db189a3f1ce5a54602197da6cc
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-05 11:46:02 +0500

    first

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 324151080 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    error = -31.282
+    acceleration = []
+    for i in range(len(x) - 1):
+        acceleration.append((x[i + 1] - x[i] + error*2)/10)
+
+    return np.mean(acceleration) # Ваш ответ
