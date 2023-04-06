b2248dd77e2c203094e391858b2a1ef171d869c7
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-06 15:31:22 +0500

    1/error = -32

--- solution.py
+++ solution.py
@@ -8,12 +8,12 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    error = 0
+    error = -32
     acceleration = []
     for i in range(len(x) - 1):
-        acceleration.append((x[i + 1] - x[i] + error*2)/10)
+        acceleration.append((x[i + 1] - x[i])/10 + (2 / error))
 
     return np.mean(acceleration) # Ваш ответ
 
-if __name__ == '__main__':
-    print(solution(np.array([45, 56, 76, 88, 67])))
+#if __name__ == '__main__':
+#    print(solution(np.array([45, 56, 76, 88, 67])))
