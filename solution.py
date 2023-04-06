3e0b4391e92bc6cd35f12e5fa5eeec6ae2e30930
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-06 12:35:23 +0500

    убрал ошибку измерения скорости

--- solution.py
+++ solution.py
@@ -8,12 +8,12 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    error = -31.282
+    error = 0
     acceleration = []
     for i in range(len(x) - 1):
         acceleration.append((x[i + 1] - x[i] + error*2)/10)
 
-    return 1/np.mean(acceleration) # Ваш ответ
+    return np.mean(acceleration) # Ваш ответ
 
-#if __name__ == '__main__':
-#    print(solution(np.array([45, 56, 76, 88, 67])))
+if __name__ == '__main__':
+    print(solution(np.array([45, 56, 76, 88, 67])))
