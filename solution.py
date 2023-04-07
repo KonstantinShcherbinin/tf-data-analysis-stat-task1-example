4d471b4b9e8914208a319472672536e0bd1132ac
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-07 09:14:18 +0500

    ошибка -31.282 и 1/ламбда средние ускорения

--- solution.py
+++ solution.py
@@ -9,11 +9,14 @@ def solution(x: np.array) -> float:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     error = -31.282
-    acceleration = []
-    for i in range(len(x) - 1):
-        acceleration.append((x[i + 1] - x[i])/10 + (2 / error))
+    #acceleration = []
+    #for i in range(len(x) - 1):
+    #    acceleration.append((x[i + 1] - x[i])/10 + (2 / error))
 
-    return np.mean(acceleration) # Ваш ответ
+    return 1 / np.mean([(i + error)/10 for i in x]) # Ваш ответ
 
 #if __name__ == '__main__':
-#    print(solution(np.array([45, 56, 76, 88, 67])))
+#    m = np.array([32, 33, 34, 32, 31])
+#    res = solution(m)
+#    print(res)
+#    print(np.mean(sum([(i - res)**2 for i in m])))
